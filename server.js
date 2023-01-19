ed27831b68683dcb2b1a65b5665aadcdb9ee1ec9
Anshul Singla <104648921+Anshul-Singla@users.noreply.github.com>
2023-01-19 15:52:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dbconnect = require("./config/db.connect");
+const cors = require("cors");
+const userRouter = require('./users/user.router.js')
+
+let PORT = 8080;
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("<h1>LIFE IS AWESOME...</h1>");
+});
+ 
+// :::::::::::user Route:::::::::::::::::::
+app.use('/user' , userRouter)
+
+
+
+
+
+
+app.listen(PORT, async () => {
+  await dbconnect;
+  console.log(`Listening on http://localhost:${PORT}`);
+});
